a125cac6983ae1c3958fedd9d406853a9bbcfac7
Kristopher Kath <Kristopher.Kath@gmail.com>
2022-02-17 15:54:21 -0800

    Used SyncVar Hooks to run custom logic when a variable gets updated

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -2,11 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string displayName = "Missing name";
-    [SyncVar] [SerializeField] private Color playerColor = Color.black;
+
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] //SyncVar will mark variable to be updated on all clients when it is modified
+    [SerializeField] private string displayName = "Missing name";
+
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] //hook will call the given method when the value changes
+    [SerializeField] private Color playerColor = Color.black;
 
     [Server]
     public void SetDisplayName(string name) 
@@ -18,6 +27,17 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void SetPlayerColor(Color c)
     {
         playerColor = c;
+    }
+
+    //To update the color of a player
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_Color", newColor);
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
     }
 
 }
