45e46a7bdbc16d5a0cfa9eb242cc10728c695769
Kris <Kristopher.Kath@gmail.com>
2022-02-16 17:14:56 -0800

    Created a NetworkManager and NetworkPlayer class which derive from Mirror classes of type. Overrid method behaviors and worked with SyncVar and Server attributes

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("Client connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        //conn.identity is the NetworkIdentity component on the player object. This has info on the MyNetworkPlayer component.
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>(); 
+
+        player.SetDisplayName($"Player {numPlayers}");
+
+        Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        player.SetPlayerColor(color);
+
+    }
+
+
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar] [SerializeField] private string displayName = "Missing name";
+    [SyncVar] [SerializeField] private Color playerColor = Color.black;
+
+    [Server]
+    public void SetDisplayName(string name) 
+    {
+        displayName = name;
+    }
+
+    [Server]
+    public void SetPlayerColor(Color c)
+    {
+        playerColor = c;
+    }
+
+}
