c7423c17cc6a3d61334c6272fd7d49d3b86492c7
Kristopher Kath <Kristopher.Kath@gmail.com>
2022-02-18 02:27:26 -0800

    Implemented the use of [ClientRpc] & [Command] attributes to have methods called onto Server by a Client and visa versa

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -11,13 +11,16 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SerializeField] private Renderer displayColorRenderer = null;
 
 
-    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] //SyncVar will mark variable to be updated on all clients when it is modified
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] //SyncVar will mark variable to be updated on all clients when it is modified.
     [SerializeField] private string displayName = "Missing name";
 
-    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] //hook will call the given method when the value changes
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] //hook will call the given method when the value changes on each client.
     [SerializeField] private Color playerColor = Color.black;
 
-    [Server]
+    #region ServerLogic
+
+    //Flow for CMD once CmdSetDisplayName is called: -- 2 --
+    [Server] //Server attribute makes it so only server machines can run these methods. No client can run this.
     public void SetDisplayName(string name) 
     {
         displayName = name;
@@ -29,15 +32,49 @@ public class MyNetworkPlayer : NetworkBehaviour
         playerColor = c;
     }
 
+
+    //Flow for CMD once CmdSetDisplayName is called: -- 1 --
+    //This is a client called function, where it is run on the server
+    [Command] //Command is an attribute that marks a method so that it can be called by a client. The server then will run its own logic
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        //Do server validation here!
+        RpcLogNewName(newDisplayName); //This is a ClientRpc method. So each client will run the methods logic to log the name
+
+        //This will set the display name and update the necesary fields on each client
+        SetDisplayName(newDisplayName); //Run the server only method
+    }
+
+    #endregion
+
+    #region ClientLogic
+
     //To update the color of a player
     private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
     {
         displayColorRenderer.material.SetColor("_Color", newColor);
     }
 
+    //Flow for CMD once CmdSetDisplayName is called: -- 3 --
     private void HandleDisplayNameUpdated(string oldName, string newName)
     {
         displayNameText.text = newName;
     }
+
+    [ContextMenu("SetMyName")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("My new name");
+    }
+
+    [ClientRpc] //This attribute has the server send a request to each client to run the following method
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
+
+
 
 }
